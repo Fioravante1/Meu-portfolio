9ffc64cb511aa77c38a6f70f35bf8b14cffa8213
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2021-08-12 16:01:01 -0300

    Feat: Cria componente do hero

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import * as S from './style';
+import fotoperfil from '../../Assets/fotoperfil.png';
+import { ReactComponent as Likedin } from '../../Assets/linkedin.svg';
+import { ReactComponent as GitHubIcon } from '../../Assets/github.svg';
+import ParticleConteiner from './Particle';
+
+function Hero() {
+  return (
+    <>
+      <S.HeroStyled>
+        <ParticleConteiner />
+      </S.HeroStyled>
+
+      <S.ContainerHero>
+        {/* <S.PhotoHero>
+          <img src={fotoperfil} alt="Fioravante" />
+        </S.PhotoHero> */}
+        <br/>
+        <br/>
+        <S.NameHero>
+          FIORAVANTE CHIOZZI
+        </S.NameHero>
+        <br/>
+        <S.ProfessionHero>
+          Desenvolvedor Web, Eterno Estudante
+        </S.ProfessionHero>
+        <br />
+      <div>
+        <a
+          href="https://github.com/Fioravante1"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <GitHubIcon />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/fioravantechiozzi/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Likedin />
+        </a>
+      </div>
+      </S.ContainerHero>
+    </>
+  )
+}
+
+export default Hero
